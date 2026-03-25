4335fa2f0147e5efccc8113c920b3a7eba50a043
SleekN <ikerperezmata03@gmail.com>
2026-03-25 18:42:22 +0100

    Initial commit

---
+++ src/main/kotlin/com/propi/propiapi/PropiApiApplication.kt
@@ -0,0 +1,11 @@
+package com.propi.propiapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PropiApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<PropiApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/propi/propiapi/PropiApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.propi.propiapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PropiApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
